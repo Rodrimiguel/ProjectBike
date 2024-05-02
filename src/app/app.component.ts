116d346b80bddac3be447bfc35e6eb6292fd6168
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-05-02 13:04:50 -0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AutosService } from './services/autos.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ProjectBike';
+
+  constructor(private autosService : AutosService){// INYECTAMOS EL CONSTRUCTOR. -- NO LLAMAS AL CONSTRUCTOR (ACA ESTAMOS INYECTAMOS LA DEPENDENCIA)
+    console.log(autosService.getAutos());// A PARTIR DE ACA SERIA LLEVAR ESTA RESPUESTA AL HTML.
+// Delegar (inject dependency)
+  }
+  
+// COMO A UN COMPONENTE INVOCAR O INYECTAR UN SERVICIO, Y QUE ESE SERVICIO NOS TRAIGA UNA DETERMINADA INFORMACION.
+// LUEGO SE HACE EL BLINDEO Y SE LLEVA AL HTML.
+
 }
---
+++ src/app/models/auto.ts
@@ -0,0 +1,5 @@
+export interface Auto { // MAPEO CON JASON (AUTOS.SERCICE.TS) CON ESTA INTERFAZ
+    id: number;
+    nombre: string;
+    modelos: string[];
+}
